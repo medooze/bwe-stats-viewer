c65e49af79e1076925e5e620aa0fe2b019f5f4ba
Brendon Costa <126128098+bcostdolby@users.noreply.github.com>
2025-09-01 11:16:14 +1000

    Add extra fields support for layer selection status (#4)

--- js/displayer.js
+++ js/displayer.js
@@ -29,7 +29,7 @@ class Accumulator
 		return this.accumulated;
 	}
 };
-const colors = ["#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#000000"]
+const colors = ["#E69F00", "#56B4E9", "#009E73", "#65118fff", "#0072B2", "#D55E00", "#CC79A7", "#000000", "#CC00FF"]
 
 const Metadata = {
 	fb			: 0,
@@ -43,7 +43,7 @@ const Metadata = {
 	delta			: 8,
 	deltaAcumulated		: 9,
 	deltaInstant		: 10,
-	bwe			: 11,
+	estimated			: 11,
 	targetBitrate		: 12,
 	availableBitrate	: 13,
 	rtt			: 14,
@@ -53,6 +53,10 @@ const Metadata = {
 	rtx			: 18,
 	probing			: 19,
 	state			: 20,
+	trackId			: 21,
+	encodingId		: 22,
+	layerBitrate	: 23,
+	layerTargetBitrate	: 24,
 	lost			: "lost",
 	delay			: "delay",
 	target			: "target",
@@ -64,7 +68,8 @@ const Metadata = {
 	bitrateProbing		: "bitrateProbing",
 	ts			: "ts",
 	fbDelay			: "fbDelay",
-	
+	trackNumber     : "trackNumber",
+	encodingNumber     : "encodingNumber"
 };
 const data = [];
 
@@ -88,13 +93,19 @@ function Process (csv)
 	let first = true;
 	let firstInInterval;
 	let count = 0;
+
+	let trackNames = new Set();
+	let layerNames = new Set();
+
+	let layerMaxTargetBitrate = new Map();
+
 	//Convert each line to array
 	for (let ini = 0, end = csv.indexOf ("\n", ini); end != -1; ini = end + 1, end = csv.indexOf ("\n", ini))
 	{
 		//get line
 		const line = csv.substr (ini, end - ini).trim ();
 		//Get data point
-		const point = line.split ("|").map (Number);
+		const point = line.split ("|").map (v => isNaN(Number(v)) ? v : Number(v));
 		//One more packet
 		packetsSent.accumulate(point[Metadata.sent],1);
 		//Check if it was lost
@@ -146,12 +157,62 @@ function Process (csv)
 		point[Metadata.fbDelay] = (point[Metadata.fb] - point[Metadata.sent])/1000;
 		//append to data
 		data.push (point);
+
+		// Collect all names
+		if (Metadata.trackId in point)
+		{
+			trackNames.add(point[Metadata.trackId]);
+
+			let encodingId = point[Metadata.encodingId];
+			let layerTargetBitrate = point[Metadata.layerTargetBitrate];
+			if (layerTargetBitrate == 0)
+			{
+				layerTargetBitrate = point[Metadata.layerBitrate];
+			}
+
+			let currentMaxBitrate = layerMaxTargetBitrate.get(encodingId);
+			layerMaxTargetBitrate.set(encodingId, currentMaxBitrate ? Math.max(currentMaxBitrate, layerTargetBitrate) : layerTargetBitrate);
+		}
+
 		//Store last feedback packet number
 		lastFeedbackNum = point[Metadata.feedbackNum];
 		first = false;
 		//Inc count
 		count ++;
 	}
+
+	// Collecl all track names and assign a number to each one
+	let trackNumber = 0;
+	let trackNumberMap = new Map();
+	for (let trackName of trackNames)
+	{
+		trackNumberMap.set(trackName, trackNumber);
+		trackNumber++;
+	}
+
+	// Sort layers by target bitrate
+	let layerMaxTargetBitrates = Array.from(layerMaxTargetBitrate).sort((a, b) => a[1] - b[1]);
+
+	// Collect all layer names and assign a number to each one
+	let encodingNumber = 0;
+	let encodingNumberMap = new Map();
+	for (let entry of layerMaxTargetBitrates)
+	{
+		encodingNumberMap.set(entry[0], encodingNumber);
+		encodingNumber++;
+	}
+
+	// If it is new version
+	if (trackNumber > 0)
+	{
+		// Add fields for the track and encoding number for display
+		for (let point of data)
+		{
+			point[Metadata.trackNumber] = trackNumberMap.get(point[Metadata.trackId]);
+			point[Metadata.encodingNumber] = encodingNumberMap.get(point[Metadata.encodingId]);
+		}
+	}
+
 	//Fix delta up to now
 	for(let i=firstInInterval; i<count; ++i)
 		//Set base delay to 0
@@ -182,6 +243,8 @@ function DisplayData (name,csv)
 	const data = window.data = Process (csv);
 	const preview = window.preview = [];
 	
+	let hasExtraFields = data.length > 0 && Metadata.trackNumber in data[0];
+
 	for (const point of data)
 		if (!preview.length || point[Metadata.sent]-preview[preview.length-1][Metadata.sent]>1000000)
 			preview.push(point);
@@ -245,13 +308,29 @@ function DisplayData (name,csv)
 	// Create charts map
 
 	//Create all charts
-	for (const id of ["ms", "mbps"])
+	let chartIds = ["ms", "mbps"];
+	if (hasExtraFields)
+	{
+		chartIds.unshift("layers");
+	}
+
+	for (const id of chartIds)
 	{
 		//Chreate new chart
 		const chart = container.createChild (am4charts.XYChart);
 		//Set padding
 		chart.padding (10, 15, 10, 15);
 		chart.margin (10, 15, 10, 15);
+
+		if (id == "layers")
+		{
+			chart.height = am4core.percent (20);
+		}
+		else
+		{
+			chart.height = am4core.percent (40);
+		}
+
 		//Use utc time
 		chart.dateFormatter.utc = true;
 		//Create legend
@@ -420,8 +499,8 @@ function DisplayData (name,csv)
 		
 		let i = 0;
 		//Create all the series
-		createBitrateSerie("Long Term BWE"	, Metadata.bwe			, colors[i++]);
-		createBitrateSerie("BWE"		, Metadata.available		, colors[i++]);
+		createBitrateSerie("Estimated"	, Metadata.estimated			, colors[i++]);
+		createBitrateSerie("Available"	, Metadata.available		, colors[i++]);
 		createBitrateSerie("Target"		, Metadata.target		, colors[i++]);
 		createBitrateSerie("Total Sent"		, Metadata.bitrateSent		, colors[i++]);
 		createBitrateSerie("Total Received"	, Metadata.bitrateRecv		, colors[i++]);
@@ -447,6 +526,22 @@ function DisplayData (name,csv)
 		stateAxis.min = stateAxis.minDefined = 0;
 		stateAxis.max = stateAxis.maxDefined = 6;
 
+		// ideally we will also change the tooltip text but not sure yet how to do this mapping as some kind of custom function instead of using tooltipText
+		stateAxis.renderer.labels.template.adapter.add("text", (label, target, key) => {
+			if (target.dataItem)
+			{
+				const v = target.dataItem.values.value.value;
+				if (v === 0) return '[black] Initial(0)';
+				else if (v === 1) return '[green] Increase(1)';
+				else if (v === 2) return '[blue] OverShoot(2)';
+				else if (v === 3) return '[orange] Congestion(3)';
+				else if (v === 4) return '[black] Recovery(4)';
+				else if (v === 5) return '[red] Loosy(5)';
+			}
+			return label;
+		});
+
+
 		function createStateSeries(name,field,colorValue)
 		{
 			//create color
@@ -467,7 +562,7 @@ function DisplayData (name,csv)
 			return serie;
 		}
 		
-		createStateSeries("state"		, Metadata.state, "#FF0000");
+		createStateSeries("state"		, Metadata.state, "#993333");
 	}
 
 	//Create lost series and axis
@@ -553,6 +648,92 @@ function DisplayData (name,csv)
 		createMSSeries("Delta acumulated", Metadata.deltaAcumulated	, colors[i++]);
 		createMSSeries("Detla instant"	, Metadata.deltaInstant		, colors[i++]);
 		
+	}
+
+	if (hasExtraFields)
+	{
+		// Reset color
+		let i = 0;
+
+		//Create layers chart
+		{
+			//Get layers chart
+			const chart = charts.layers;
+			//Create axis
+			var mbpsAxis = chart.yAxes.push (new am4charts.ValueAxis ());
+			mbpsAxis.renderer.labels.template.fill = am4core.color (colorHash.hex ("mbpsAxis"));
+			mbpsAxis.numberFormatter = new am4core.NumberFormatter ();
+			mbpsAxis.numberFormatter.numberFormat = "#.###a'bps'";
+			mbpsAxis.renderer.maxWidth = mbpsAxis.renderer.minWidth = 120;
+			mbpsAxis.renderer.grid.template.strokeOpacity = 0.07;
+			mbpsAxis.tooltip.disabled = true;
+
+			function createBpsSeries(name,field,colorValue)
+			{
+				//create color
+				const color = am4core.color(colorValue || colorHash.hex ("medooze"+name));
+				//Create serie
+				const serie = chart.series.push (new am4charts.LineSeries ());
+				serie.name = name;
+				serie.dataFields.dateX = Metadata.ts;
+				serie.dataFields.valueY = field;
+				serie.yAxis = mbpsAxis;
+				serie.tooltipText = "{name}: {valueY.formatNumber(\"#.###a'bps'\")}";
+				serie.fill = color;
+				serie.stroke = color;
+				serie.startLocation = 0;
+				serie.connect = false;
+				serie.autoGapCount = 100;
+				//Done
+				return serie;
+			}
+			
+			createBpsSeries("layerBitrate", Metadata.layerBitrate, colors[i++]);
+			createBpsSeries("layerTargetBitrate", Metadata.layerTargetBitrate, colors[i++]);	
+		}
+
+		//Create track/layer names series and axis
+		{
+			//Get layers chart
+			const chart = charts.layers;
+			//Create axis
+			var layerAxis = chart.yAxes.push (new am4charts.ValueAxis());
+			layerAxis.renderer.labels.template.fill = am4core.color(colorHash.hex("State"));
+			layerAxis.numberFormatter = new am4core.NumberFormatter ();
+			layerAxis.numberFormatter.numberFormat = "#";;
+			layerAxis.renderer.labels.template.fill = am4core.color (colorHash.hex ("state"));
+			layerAxis.renderer.maxWidth = layerAxis.renderer.minWidth = 120;
+			layerAxis.renderer.opposite = true;
+			layerAxis.renderer.grid.template.strokeOpacity = 0.07;
+			layerAxis.tooltip.disabled = true;
+			layerAxis.min = layerAxis.minDefined = 0;
+			layerAxis.max = layerAxis.maxDefined = 6;
+
+			function createLayersSeries(name,valueField,colorValue,nameField)
+			{
+				//create color
+				const color = am4core.color(colorValue || colorHash.hex ("medooze"+name));
+				//Create serie
+				var serie = chart.series.push (new am4charts.LineSeries ());
+				serie.name = name;
+				serie.dataFields.dateX = Metadata.ts;
+				serie.dataFields.valueY = valueField;
+				serie.dataFields.nameY = nameField;
+				serie.yAxis = layerAxis;
+				serie.tooltipText = "{name}: {nameY}";
+				serie.fill = color;
+				serie.stroke = color;
+				serie.startLocation = 0;
+				serie.connect = false;
+				serie.autoGapCount = 100;
+				//Done
+				return serie;
+			}
+			
+			createLayersSeries("trackId"		, Metadata.trackNumber, colors[i++], Metadata.trackId);
+			createLayersSeries("encodingId"		, Metadata.encodingNumber, colors[i++],  Metadata.encodingId);
+		}
+
 	}
 }
 
